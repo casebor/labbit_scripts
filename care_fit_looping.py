ecd945f96cef40394fc3936eedb252d3f30fe610
Carlos <casebor@gmail.com>
2018-03-16 17:51:16 +0100

    Modified pymol script to work without argparser :(

--- care_fit_looping.py
+++ care_fit_looping.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  pymol_fit_looping.py
+#  care_fit_looping.py
 #  
 #  Copyright 2018 Carlos Eduardo Sequeiros Borja <casebor@gmail.com>
 #  
@@ -22,27 +22,65 @@
 #  
 #  
 
-import argparse
+import sys, os
+
+# autocompletion
+import readline
+import rlcompleter
+readline.parse_and_bind('tab: complete')
+
+# pymol launching: quiet (-q), without GUI (-c) and with arguments from command line
+import pymol
 from pymol import cmd
+args = sys.argv[1:]
+pymol.pymol_argv = ['pymol','-qc'] + args
+pymol.finish_launching()
 
-def main():
-    """ Main function """
-    protein = cmd.load(args.ref)
-    loop = cmd.load(args.infile)
-    
-    prt_s_1 = 
-    prot_s = cmd.select("prot_start", "/%s//%s/%d+%d/N+CA+C+O" %(args.ref.split(".")[0], args.chain, args.bounds[0]-2, args.bounds[0]-1))
-    prot_e = cmd.select("prot_end",   "/%s//%s/%d+%d/N+CA+C+O" %(args.ref.split(".")[0], args.chain, args.bounds[1]+1, args.bounds[1]+2))
-    loop_s = cmd.select("loop_start", "/%s///%d+%d/N+CA+C+O" %(args.ref.split(".")[0], 1, 2))
-    loop_e = cmd.select("loop_end",   "/%s///%d+%d/N+CA+C+O" %(args.ref.split(".")[0], args.bounds[1]-args.bounds[0]+3, , args.bounds[1]-args.bounds[0]+4))
-    cmd.pair_fit()
+if '-h' in args:
+    print("help")
+else:
+    if ('-r' in args) and ('-i' in args) and ('-o' in args) and ('-c' in args) and ('-b' in args) and (len(args) == 11):
+        protein = args[args.index('-r')+1]
+        loop = args[args.index('-i')+1]
+        outfile = args[args.index('-o')+1]
+        chain = args[args.index('-c')+1]
+        ini = int(args[args.index('-b')+1])
+        end = int(args[args.index('-b')+2])
+        
+        cmd.load(protein)
+        cmd.load(loop)
+        p_name = os.path.basename(protein).split('.')[0]
+        l_name = os.path.basename(loop).split('.')[0]
+        print(l_name, "<--- Loop name")
+        #l_size = len(cmd.get_model(l_name).get_residues())
+        cmd.select("p_s1", "/%s//%s/%d/N"  %(p_name, chain, ini-2))
+        cmd.select("p_s2", "/%s//%s/%d/CA" %(p_name, chain, ini-2))
+        cmd.select("p_s3", "/%s//%s/%d/C"  %(p_name, chain, ini-2))
+        cmd.select("p_s4", "/%s//%s/%d/N"  %(p_name, chain, ini-1))
+        cmd.select("p_s5", "/%s//%s/%d/CA" %(p_name, chain, ini-1))
+        cmd.select("p_s6", "/%s//%s/%d/C"  %(p_name, chain, ini-1))
+        cmd.select("p_e1", "/%s//%s/%d/N"  %(p_name, chain, end+1))
+        cmd.select("p_e2", "/%s//%s/%d/CA" %(p_name, chain, end+1))
+        cmd.select("p_e3", "/%s//%s/%d/C"  %(p_name, chain, end+1))
+        cmd.select("p_e4", "/%s//%s/%d/N"  %(p_name, chain, end+2))
+        cmd.select("p_e5", "/%s//%s/%d/CA" %(p_name, chain, end+2))
+        cmd.select("p_e6", "/%s//%s/%d/C"  %(p_name, chain, end+2))
+        cmd.select("l_s1", "/%s///%d/N"  %(l_name, ini-2))
+        cmd.select("l_s2", "/%s///%d/CA" %(l_name, ini-2))
+        cmd.select("l_s3", "/%s///%d/C"  %(l_name, ini-2))
+        cmd.select("l_s4", "/%s///%d/N"  %(l_name, ini-1))
+        cmd.select("l_s5", "/%s///%d/CA" %(l_name, ini-1))
+        cmd.select("l_s6", "/%s///%d/C"  %(l_name, ini-1))
+        cmd.select("l_e1", "/%s///%d/N"  %(l_name, end+1))
+        cmd.select("l_e2", "/%s///%d/CA" %(l_name, end+1))
+        cmd.select("l_e3", "/%s///%d/C"  %(l_name, end+1))
+        cmd.select("l_e4", "/%s///%d/N"  %(l_name, end+2))
+        cmd.select("l_e5", "/%s///%d/CA" %(l_name, end+2))
+        cmd.select("l_e6", "/%s///%d/C"  %(l_name, end+2))
+        out_loop = cmd.select("out_loop", "/%s////" %(l_name))
+        cmd.pair_fit("l_s1","p_s1", "l_s2","p_s2", "l_s3","p_s3", "l_s4","p_s4", "l_s5","p_s5", "l_s6","p_s6",
+                     "l_e1","p_e1", "l_e2","p_e2", "l_e3","p_e3", "l_e4","p_e4", "l_e5","p_e5", "l_e6","p_e6")
+        cmd.save(outfile, "out_loop")
+    else:
+        print("help")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Your script description')
-    parser.add_argument('-r', '--ref', required=True, type=argparse.FileType('r'), dest='ref', help='Input PDB protein file to fit the loop pdb (-i option).')
-    parser.add_argument('-i', '--input', required=True, type=argparse.FileType('r'), dest='infile', help='Input PDB loop file to fit onto -r option.')
-    parser.add_argument('-c', '--chain', default="A", dest='chain', help='Chain where the loop is missing in the protein (-r, --ref option).')
-    parser.add_argument('-o', '--output', required=True, type=argparse.FileType('w'), dest='outfile', help='Defines the name of the modified molecule file.')
-    parser.add_argument('-b', '--boundaries', required=True, type=int, nargs=2, dest='bounds', help='Here you define the residue number of the initial and last residue of the loop, i.e. the residues that are missing in the structure.')
-    args = parser.parse_args()
-    main()
