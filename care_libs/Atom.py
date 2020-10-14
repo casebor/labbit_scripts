2ed5a97565c852089c9f30973b43160f5a4fbfb0
casebor <casebor@gmail.com>
2020-10-14 23:44:20 +0200

    Added functions to the Octree

--- care_libs/Atom.py
+++ care_libs/Atom.py
@@ -6,8 +6,12 @@
 
 import numpy as np
 
-RADII = {"C": (1.0, 2.0, 3.0), "H": (1.0, 2.0, 3.0), "O": (1.0, 2.0, 3.0),
-         "N": (1.0, 2.0, 3.0), "P": (1.0, 2.0, 3.0), "S": (1.0, 2.0, 3.0)}
+# For each atom, we have (atoms radius, covalent radius and Van der Waals radius)
+RADII = {"C": (0.67, 0.76, 1.7), "H": (0.53, 0.31, 1.0), "O": (0.48, 0.66, 1.52),
+         "N": (0.56, 0.71, 1.55), "P": (0.98, 1.07, 1.8), "S": (0.87, 1.05, 1.8),
+         "Na": (1.9, 1.6, 2.27), "Mg": (1.45, 1.41, 1.73), "K": (2.43, 2.03, 2.75),
+         "Ca": (1.94, 1.76, 2.0), "F": (0.42, 0.57, 1.47), "Cl": (0.79, 1.02, 1.75),
+         "Br": (0.94, 1.2, 1.85), "I": (1.15, 1.39, 1.98)}
 
 def _get_radius(name):
     pass
@@ -24,10 +28,10 @@ class Atom:
                   res_name=None, chain='X', atomic_number=0, occupancy=0.0,
                   bfactor=0.0, charge=0.0, bonds_indices=None):
         if pos is None:
-            pos = (0.0, 0.0, 0.0)
+            pos = [0.0, 0.0, 0.0]
         self.name = name
         self.index = index
-        self.pos = np.array((pos[0], pos[1], pos[2]))
+        self.pos = np.array(pos)
         self.res_index = res_index
         self.res_name = res_name
         self.chain = chain
@@ -43,4 +47,15 @@ class Atom:
         """ Function doc """
         return self.pos
     
-
+    def x(self):
+        return self.pos[0]
+    
+    def y(self):
+        return self.pos[1]
+    
+    def z(self):
+        return self.pos[2]
+    
+    def radius(self):
+        return self.radius
+    
--- care_libs/Octree.py
+++ care_libs/Octree.py
@@ -8,7 +8,113 @@ import numpy as np
 
 class Octree(Object):
     """ Class doc """
-    def __init__ (self, atoms, ):
+    # def __init__ (self, atoms, origin=None, top_left_front=None,
+    #               bottom_right_back=None):
+    def __init__ (self, atoms=None, origin=None, border=None):
         super().__init__(self)
+        if atoms is None:
+            atoms = []
+        self.to_insert = atoms
+        if origin is None:
+            origin = [0., 0., 0.]
+        self.origin = np.array(origin)
+        # if top_left_front is None:
+        #     top_left_front = [999., 999., 999]
+        # self.top_left_front = top_left_front
+        # if bottom_right_back is None:
+        #     bottom_right_back = [-999., -999., -999.]
+        if border is None:
+            border = 512.
+        self.border = int(border) + 1
+        self.bottom_right_back = bottom_right_back
+        self.elements = []
+        self.children = [None] * 8
+        self.is_leaf = False
+    
+    def initialize(self):
+        if abs(self.border - self.origin) <= 4.0:
+            self.is_leaf = True
+        if self.is_leaf:
+            self.elements = [e for e in self.to_insert]
+            # self.calculate_collisions()
+        else:
+            while self.to_insert:
+                atom = self.to_insert.pop()
+                if abs(atom.x() - self.origin[0]) < atom.radius():
+                    self.elements.append(atom)
+                elif abs(atom.y() - self.origin[1]) < atom.radius():
+                    self.elements.append(atom)
+                elif abs(atom.z() - self.origin[2]) < atom.radius():
+                    self.elements.append(atom)
+                else:
+                    self.add_atom_to_children(atom)
+    
+    def add_atom_to_children(self, atom):
+        child_pos = np.abs(atom.coords())
+        child_pos = np.array(atom.coords()/child_pos, dtype=np.int)
+        child_pos = np.array(child_pos+1, dtype=np.bool)
+        if child_pos[0] and child_pos[1] and child_pos[2]:
+            if self.children[0] is None:
+                _origin = self.origin + self.border
+                _border = self.border / 2.
+                self.children[0] = Octree(origin=_origin, border=_border)
+            else:
+                self.children[0].add_temp(atom)
+        elif child_pos[0] and child_pos[1] and not child_pos[2]:
+            if self.children[1] is None:
+                _origin = self.origin + [self.border, self.border, -self.border]
+                _border = self.border / 2.
+                self.children[1] = Octree(origin=_origin, border=_border)
+            else:
+                self.children[1].add_temp(atom)
+        elif not child_pos[0] and child_pos[1] and not child_pos[2]:
+            if self.children[2] is None:
+                _origin = self.origin + [-self.border, self.border, -self.border]
+                _border = self.border / 2.
+                self.children[2] = Octree(origin=_origin, border=_border)
+            else:
+                self.children[2].add_temp(atom)
+        elif not child_pos[0] and child_pos[1] and child_pos[2]:
+            if self.children[3] is None:
+                _origin = self.origin + [-self.border, self.border, self.border]
+                _border = self.border / 2.
+                self.children[3] = Octree(origin=_origin, border=_border)
+            else:
+                self.children[3].add_temp(atom)
+        elif child_pos[0] and not child_pos[1] and child_pos[2]:
+            if self.children[4] is None:
+                _origin = self.origin + [self.border, -self.border, self.border]
+                _border = self.border / 2.
+                self.children[4] = Octree(origin=_origin, border=_border)
+            else:
+                self.children[4].add_temp(atom)
+        elif child_pos[0] and not child_pos[1] and not child_pos[2]:
+            if self.children[5] is None:
+                _origin = self.origin + [self.border, -self.border, -self.border]
+                _border = self.border / 2.
+                self.children[5] = Octree(origin=_origin, border=_border)
+            else:
+                self.children[5].add_temp(atom)
+        elif not child_pos[0] and not child_pos[1] and not child_pos[2]:
+            if self.children[6] is None:
+                _origin = self.origin + [-self.border, -self.border, -self.border]
+                _border = self.border / 2.
+                self.children[6] = Octree(origin=_origin, border=_border)
+            else:
+                self.children[6].add_temp(atom)
+        elif not child_pos[0] and not child_pos[1] and child_pos[2]:
+            if self.children[7] is None:
+                _origin = self.origin + [-self.border, -self.border, self.border]
+                _border = self.border / 2.
+                self.children[7] = Octree(origin=_origin, border=_border)
+            else:
+                self.children[7].add_temp(atom)
+    
+    def add_temp(self, atom):
+        self.to_insert.append(atom)
+    
+    def calculate_collisions(self):
+        pass
+    
     
 
